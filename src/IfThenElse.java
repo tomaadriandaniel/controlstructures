6dd3b8165b992aac281237fd3ec8f598a10573c7
adi <toma_adrian27@yahoo.com>
2022-08-01 16:22:44 +0300

    Primul upload

---
+++ src/IfThenElse.java
@@ -0,0 +1,27 @@
+public class IfThenElse {
+    public static void main(String[] args) {
+                int a,b,c;
+                        b=25;
+                        c=28;
+                        a= b + c;
+
+
+        if(a!=50){
+            System.out.println("false");
+        }
+        else{
+            System.out.println("true");
+
+        }
+
+
+        for (int i = 0; i<=50; i= i+2) {
+            System.out.println("valoarea lui i este: " + i);
+        }
+
+        }
+
+    }
+
+
+
